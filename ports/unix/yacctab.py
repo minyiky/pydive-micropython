0d75eaf62a54c8e82aeb0f45cc2be07199d6c28b
Michael McDonnell <mmcdonnell@hotmail.co.uk>
2025-08-17 18:47:21 +0100

    fix lint issues

--- ports/unix/yacctab.py
+++ ports/unix/yacctab.py
@@ -12,7 +12,8 @@ _lr_action_items = {'$end':([0,1,2,3,4,5,6,7,8,9,10,14,15,63,89,90,125,205,248,2
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
    for _x,_y in zip(_v[0],_v[1]):
-      if not _x in _lr_action:  _lr_action[_x] = {}
+      if _x not in _lr_action: 
+         _lr_action[_x] = {}
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
@@ -21,8 +22,9 @@ _lr_goto_items = {'translation_unit_or_empty':([0,],[1,]),'translation_unit':([0
 _lr_goto = {}
 for _k, _v in _lr_goto_items.items():
    for _x, _y in zip(_v[0], _v[1]):
-       if not _x in _lr_goto: _lr_goto[_x] = {}
-       _lr_goto[_x][_k] = _y
+      if _x not in _lr_goto:
+         _lr_goto[_x] = {}
+      _lr_goto[_x][_k] = _y
 del _lr_goto_items
 _lr_productions = [
   ("S' -> translation_unit_or_empty","S'",1,None,None,None),
