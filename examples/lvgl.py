9c37c75637e63a165311b60d721769daeeafe05e
minyiky <71966069+minyiky@users.noreply.github.com>
2025-09-13 15:52:15 +0100

    Add LVGL submodule to unix port and tidy up actions (#1)
    I have added in the `lv_micropython_bindings` as a new submodule under
    `user_modules` to allow firmware built this way to be used in the unix
    simulator.
    
    Additionally I have updated some actions with the following changes
     - Removed unneeded actions
     - Added PR Hygiene actions
     - Added a container build action
    - Added auto tagging of the repo (may change later to follow mp
    versioning)

---
+++ examples/lvgl.py
@@ -0,0 +1,23 @@
+"""
+A basic example that registers SDL display and input drivers and starts the event loop
+"""
+
+import time
+
+import lvgl as lv
+from lv_utils import event_loop
+
+lv.init()
+
+el = event_loop()
+
+WIDTH = 480
+HEIGHT = 320
+
+disp_drv = lv.sdl_window_create(WIDTH, HEIGHT)
+mouse = lv.sdl_mouse_create()
+keyboard = lv.sdl_keyboard_create()
+keyboard.set_group(lv.group_create())
+
+while True:
+    time.sleep(100)
